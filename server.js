12bb51b9fc142ea1634cd30d450f3a46004bb116
fuaberu <kevinfabel@gmail.com>
2021-11-18 18:09:23 -0300

    .env var test

--- server.js
+++ server.js
@@ -87,6 +87,10 @@ app.get('/api/shorturl/:url', async function (req, res) {
 	}
 });
 
+app.get('/api', async function (req, res) {
+	res.json({ var: process.env.MONGOURL });
+});
+
 app.listen(port, function () {
 	console.log(`Listening on port ${port}`);
 });
